6f39550c3c0b713925609c8e1ebd5a30a11e10e0
Akbar <akbar.erken@gmail.com>
2020-12-02 11:54:27 +0100

    finished exercise except last one

--- Week1/project/index.js
+++ Week1/project/index.js
@@ -1,1 +1,28 @@
 // your code goes in here
+
+const quotes = [
+  {
+    quote: 'Action speaks loader than words',
+    author: 'Mark Twain',
+  },
+  {
+    quote: 'You talk when you cease to be at peace with your thoughts.',
+    author: 'Kahlil Gibran',
+  },
+  {
+    quote: 'Beauty is not in the face; beauty is a light in the heart..',
+    author: 'Kahlil Gibran',
+  },
+  {
+    quote: 'We are all like the bright moon, we still have our darker side.',
+    author: 'Kahlil Gibran',
+  },
+  {
+    quote: 'Generosity is giving more than you can, and pride is taking less than you need.',
+    author: 'Kahlil Gibran',
+  },
+  {
+    quote: "Don't let yesterday take up too much of today.",
+    author: ' Will Rogers',
+  },
+];
