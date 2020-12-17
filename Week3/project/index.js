30b3ad1f7d0aba184b021083bc748a888065b80f
Akbar <akbar.erken@gmail.com>
2020-12-17 11:55:02 +0100

    fixed some errors

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -11,17 +11,18 @@ header.innerText = 'TIP CALCULATOR';
 app.appendChild(header);
 
 //create main section
-//first part
+
+//bill amount part
 const bill = document.createElement('h2');
 bill.setAttribute('class', 'bill');
 const billInput = document.createElement('input');
 billInput.setAttribute('class', 'billInput');
-bill.innerText = 'how much was your bill?';
+bill.innerText = 'How much was your bill?';
 
 app.appendChild(bill);
 app.appendChild(billInput);
 
-// second part
+// service select part
 const service = document.createElement('h3');
 service.setAttribute('class', 'service');
 const serviceSelect = document.createElement('select');
@@ -45,4 +46,55 @@ serviceSelect.appendChild(option3);
 app.appendChild(service);
 app.appendChild(serviceSelect);
 
-//third part
+//sharing the bill part
+
+const share = document.createElement('h3');
+share.innerText = 'How many people are sharing the bill?';
+share.setAttribute('class', 'share');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+const peopleInput = document.createElement('input');
+peopleInput.setAttribute('class', 'peopleInput');
+const people = document.createElement('people');
+people.setAttribute('class', 'people');
+people.innerText = 'people';
+
+app.appendChild(share);
+container.appendChild(peopleInput);
+container.appendChild(people);
+app.appendChild(container);
+
+// last caculation result part
+
+const calculateBtn = document.createElement('button');
+calculateBtn.setAttribute('id', 'btn');
+calculateBtn.innerText = 'CALCULATE!';
+
+const tipAmount = document.createElement('tipAmount');
+tipAmount.setAttribute('class', 'tip');
+tipAmount.innerText = 'TIP AMOUNT';
+const resultContainer = document.createElement('num');
+resultContainer.setAttribute('class', 'result');
+resultContainer.innerText = 'xx';
+
+const each = document.createElement('each');
+each.setAttribute('class', 'each');
+each.innerText = 'each';
+
+app.appendChild(calculateBtn);
+app.appendChild(tipAmount);
+
+app.appendChild(each);
+
+calculateBtn.addEventListener('click', calculation);
+
+function calculation() {
+  const billTotal = Number(billInput.value);
+  const serviceRate = Number(serviceSelect.value);
+  const peopleNum = Number(peopleInput.value);
+
+  tipResult = (billTotal * serviceRate) / peopleNum;
+  fixedTipResult = tipResult.toFixed(2);
+  resultContainer.innerText = `$ ${fixedTipResult}`;
+  app.appendChild(resultContainer);
+}
